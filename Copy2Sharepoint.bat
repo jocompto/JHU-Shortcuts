0f38537940b06f31fac04b6b958c90c5a94b6cd0
Joe Compton <jcompto8@jhu.edu>
2016-06-10 12:19:02 -0400

    BatchSynch
    Created a batch file to sync the local copy to the Sharepoint directory.

---
+++ Copy2Sharepoint.bat
@@ -0,0 +1,3 @@
+net use s: "\\collaborate.johnshopkins.edu@SSL\DavWWWRoot\sites\SAP\SupportTeam\Dev\Shared Documents\QuickLinks"
+copy C:\Users\jcompto8\Documents\GitHub\JHU-Shortcuts\Shortcuts.htm  s:
+net use /DELETE S:
