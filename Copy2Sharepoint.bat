c783325d374fc69b6ecbfd41c046d32161d156ca
Joe Compton <jcompto8@jhu.edu>
2016-06-16 19:05:24 -0400

    CollectiveUpdate
    URL Changes/fixes to Shortcuts.htm;  Additional command switches to
    Copy2Sharepoint; New image file

--- Copy2Sharepoint.bat
+++ Copy2Sharepoint.bat
@@ -1,3 +1,5 @@
 net use s: "\\collaborate.johnshopkins.edu@SSL\DavWWWRoot\sites\SAP\SupportTeam\Dev\Shared Documents\QuickLinks"
+REM xcopy C:\Users\jcompto8\Documents\GitHub\JHU-Shortcuts\* s: /EXCLUDE: Copy2Sharepoint.bat
 copy C:\Users\jcompto8\Documents\GitHub\JHU-Shortcuts\Shortcuts.htm  s:
+xcopy C:\Users\jcompto8\Documents\GitHub\JHU-Shortcuts\*.* s: /E /Y/C /EXCLUDE:*.bat+.git*
 net use /DELETE S:
